978070feff7dad0acc0a5d648219e40efb68e3d8
Ucha Sephiskveradze <ucha.sephiskveradze@gmail.com>
2024-03-25 19:50:53 +0400

    ASNDKOASLJDKLSAJDLKASJD SHEVISHLEBI SALDNAJKLSDNLKASDNKLASOJ SHEVISHALE ASDJLAKSDJALSKD

--- src/app/password-validator/password-validator.component.ts
+++
@@ -1,8 +0,0 @@
-import { AbstractControl } from '@angular/forms';
-
-export const passwordConfirmation = (control: AbstractControl) => {
-  let password = control.get('password');
-  let confirmPassword = control.get('confirmPassword');
-  let doesntMatch = password?.value !== confirmPassword?.value;
-  return doesntMatch ? { doesntMatch: true } : null;
-};
--- src/app/signup-page/signup-form/signup-form.component.ts
+++ src/app/signup-page/signup-form/signup-form.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import {
+  AbstractControl,
   FormBuilder,
   FormGroup,
+  ValidatorFn,
   Validators,
 } from '@angular/forms';
 import { Router } from '@angular/router';
-import { passwordConfirmation } from 'src/app/password-validator/password-validator.component';
+import { confirmPasswordValidator } from 'src/app/signup-page/signup-form/password-validator/password-validator.component';
 
 @Component({
   selector: 'app-signup-form',
@@ -16,47 +18,50 @@ export class SignupFormComponent implements OnInit {
 
   signUpForm: FormGroup;
   isButtonActive: boolean = false;
-confirmPassword: any;
-  constructor(private sf: FormBuilder, private router: Router) {
-    this.signUpForm = this.sf.group(
-      {
-        firstName: [
-          '',
-          [
-            Validators.required,
-            Validators.minLength(3),
-            Validators.maxLength(17),
-          ],
-        ],
-        lastName: [
-          '',
-          [
-            Validators.required,
-            Validators.minLength(3),
-            Validators.maxLength(17),
-          ],
-        ],
-        email: ['', Validators.email],
-        mobile: ['', [Validators.required, Validators.pattern('995[0-9]{9}')]],
-        password: [
-          '',
-          [
-            Validators.required,
-            Validators.minLength(6),
-            Validators.pattern(
-              '^(?=.*[A-Z])(?=.*[!@#$%^&*(),.?":{}|<>])(?=.*[a-zA-Z])[0-9A-Za-z!@#$%^&*(),.?":{}|<>]+$'
-            ),
-          ],
-        ],
-        confirmPassword: ['', Validators.required],
-        companyName: ['', Validators.required],
-      },
-      {
-        validators: [passwordConfirmation],
-      }
-    );
-  }
-  ngOnInit(): void {}
+constructor(private sf: FormBuilder, private router: Router) {
+  this.signUpForm = this.sf.group({
+    firstName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(17)]],
+    lastName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(17)]],
+    email: ['', Validators.email],
+    mobile: ['', [Validators.required, Validators.pattern('995[0-9]{9}')]],
+    password: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.pattern(
+          '^(?=.*[A-Z])(?=.*[!@#$%^&*(),.?":{}|<>])(?=.*[a-zA-Z])[0-9A-Za-z!@#$%^&*(),.?":{}|<>]+$'
+        ),
+      ],
+    ],
+    confirmPassword: ['', [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.pattern(
+        '^(?=.*[A-Z])(?=.*[!@#$%^&*(),.?":{}|<>])(?=.*[a-zA-Z])[0-9A-Za-z!@#$%^&*(),.?":{}|<>]+$'
+      )]],
+    companyName: ['', Validators.required],
+  },
+  { validators: confirmPasswordValidator },
+  );
+}
+  // passwordMatchValidator(control: AbstractControl){
+  //   return control.get('password')?.value === control.get('confirmPassword')?.value 
+  //   ? null 
+  //   : {mismatch: true};
+  // }
+
+  
+  ngOnInit() {}
+
+
+passwordChecker(formGroup: FormGroup) {
+  const passwordValue = formGroup.get('password')?.value;
+  const confirmPasswordValue = formGroup.get('confirmPassword')?.value;
+
+  return passwordValue === confirmPasswordValue ? null : { passwordNotMatch: true };
+}
+
 
   get f() { return this.signUpForm.controls; }
 
@@ -76,6 +81,11 @@ confirmPassword: any;
   get password() {
     return this.signUpForm.get('password');
   }
+
+  get confirmPassword() {
+    return this.signUpForm.get('confirmPassword')
+  }
+
   get mobile() {
     return this.signUpForm.get('mobile');
   }
@@ -88,3 +98,5 @@ confirmPassword: any;
     return this.signUpForm.valid && this.isButtonActive;
   }
 }
+
+
